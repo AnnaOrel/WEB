bbb6e99ae55dd583cb0e92256ec619e403352107
Anna Orel <AKuOrel@stud.kantiana.ru>
2022-02-27 11:15:43 +0200

    Lab2-11

--- Lab2.php
+++ Lab2.php
@@ -31,4 +31,15 @@ echo "\n" . (32 - 20);
 $last_month = 1187.23;
 $this_month = 1089.98;
 echo $last_month - $this_month;
+
+echo "\n 11 \n";
+
+$num_languages = 4;
+$months = 11;
+
+$days = $months * 16;
+
+$days_per_language = $days / $num_languages;
+
+echo $days_per_language;
 ?>
