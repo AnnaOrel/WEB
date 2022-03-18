d6c05bcdfe8f98ec609e2f531eb98c5ea474cc06
Anna Orel <AKuOrel@stud.kantiana.ru>
2022-03-18 23:26:03 +0200

    Lab3-2B

---
+++ Lab3/Lab3-2B.php
@@ -0,0 +1,21 @@
+<?php
+
+	if (!isset($_POST["Name"]) || !isset($_POST["Surname"]) || !isset($_POST["Age"])) 
+	{
+		http_response_code(400);
+		return;
+	}
+
+	if (!session_start()) 
+	{
+		http_response_code(500);
+		return;
+	}
+	
+	$_SESSION["Name"] = $_POST["Name"];
+	$_SESSION["Surname"] = $_POST["Surname"];
+	$_SESSION["Age"] = $_POST["Age"];
+	
+	header("Refresh:3; url=Lab3-2B-1.php");
+?>
+
