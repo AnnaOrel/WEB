09717f0410c5a008bbdb80616f885794803e7229
Anna Orel <AKuOrel@stud.kantiana.ru>
2022-03-25 12:24:37 +0200

    Lab4

---
+++ Lab4/board.php
@@ -0,0 +1,94 @@
+<?php
+	require "/vendor/autoload.php";
+	$client = new Google_Client();
+	$client->setApplicationName('Google Sheets API PHP Quickstart');
+	$client->setScopes(Google_Service_Sheets::SPREADSHEETS_READONLY);
+	$client->setAuthConfig('credentials.json');
+	$client->setAccessType('offline');
+	$client->setPrompt('select_account consent');
+	$service = new Google_Service_Sheets($client);
+	$spreadsheetId = '13ixJlPRuhkFhySTPfW5Suu65t5iHVZYfPrTC0-y42uE';
+	$range = 'AdData!A1:D';
+	$response = $service->spreadsheets_values->get($spreadsheetId, $range);
+	$values = $response->getValues();
+	var_dump($values);
+?>
+
+<!DOCTYPE html>
+	<html lang="ru">
+	  <head>
+		<meta charset="UTF-8">
+		<title>Доска объявлений</title>
+	  </head>
+	<body>
+		<form action="/saveinfo.php" method="GET">
+			<label>
+            e-mail<br>
+            <input type="Email" name="Email"><br>
+            Category<br>
+            <select name="Categories" required>
+                <option>Headset</option>
+                <option>Mirror</option>
+                <option>Phone</option>
+            </select><br>
+            Headline<br>
+            <input type="Text" name="Headline"><br>
+            Text<br>
+            <textarea cols="40" rows="6" name="Text"></textarea>
+            <br><br>
+            <input type="Submit" name="Submit" value="add"><br><br>
+			</label>
+		</form>
+	</body>
+</html>
+
+<?php
+	$categories = scandir(__DIR__."/categories");
+	$ads = [];
+	foreach($categories as $category){
+		if ($category != "." && $category != ".."){
+			$ads[$category] = scandir("categories/$category");
+		}
+	};
+	echo "<table border=1><caption>Ads</caption>";
+	foreach($ads as $category => $arr){
+		echo "<tr><th colspan=3 align=center>" . ucfirst($category) . "</th></tr>";
+		foreach($arr as $ad)
+		{
+			if ($ad != "." && $ad != "..")
+			{
+				$file = fopen("categories/$category/$ad", "r");
+				$email = fgets($file);
+				$headline = fgets($file);
+				$Text = fgets($file);
+				echo "<tr><td>$email</td><td>$headline</td><td>$Text</td></tr>";
+			}
+		}
+	}
+	echo "</table>";
+?>
+
+
+<body>
+	<form>
+		<label>
+			<h1>Ads from GoogleSheets</h1>
+			<table border=1>
+				<tr><th>Category</th><th>Email</th><th>Header</th><th>Text</th></tr>
+				<tbody>
+
+				<?php
+				foreach($values as $adData){
+					echo "<tr>" . "<td>" . ucfirst($adData[0]) . "</td>";
+					echo "<td>" . $adData[1] . "</td>";
+					echo "<td>" . $adData[2] . "</td>";
+					echo "<td>" . $adData[3] . "</td>" . "</tr>";
+				}
+				?>
+				
+				
+				</tbody>
+			</table>
+		</label>
+	</form>
+</body>
